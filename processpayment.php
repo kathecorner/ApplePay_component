951393272006d8ededa5d2483777c90089e2768d
kathecorner <40414604+kathecorner@users.noreply.github.com>
2022-12-09 11:38:32 +0900

    Create processpayment.php

---
+++ processpayment.php
@@ -0,0 +1,101 @@
+<?php
+
+$url = "https://checkout-test.adyen.com/v69/payments";
+
+$payments_data = $_POST;
+
+//echo $payments_data;
+
+$additional_data = [
+    //'reference' => 'KenjiW001',
+    'reference' => 'playground_11142022_01',
+    'merchantAccount' => 'NobuyaIwaoCOM',
+    //'countryCode' => 'DK',
+    'amount' => [
+        'value' => 0,
+        'currency' => 'INR'
+    ],
+    //'returnUrl' => 'http://127.0.0.1:8080/return.php',
+    'returnUrl' => 'http://127.0.0.1:8080/result.php',
+    'channel' => 'Web',
+    'additionalData' => [
+        'allow3DS2' => 'true'
+    ],
+    "threeDS2RequestData"=> [
+      "threeDSCompInd"=> "Y",
+      "threeDSRequestorChallengeInd"=> "01"
+
+    ],
+    'origin' => 'http://127.0.0.1:8080',
+    'billingAddress' => '123 Eastgate, San Diego, USA, 92121',
+    //'paymentMethod' => $payments_data->PaymentMethod,
+    /*
+    'lineItems' => array(
+                 'quantity' =>'1',
+                 'taxPercentage' =>'2100',
+                 'description' =>'Shoes',
+                 'id' =>'Item #1',
+                 'amountIncludingTax' =>'400',
+                 'productUrl' => 'URL_TO_PURCHASED_ITEM',
+                 'imageUrl' => 'URL_TO_PICTURE_OF_PURCHASED_ITEM'
+                ),
+
+
+    'deliveryAddress' => array(
+                'city' => 'Singapore',
+                'country' => 'SG',
+                'houseNumberOrName' => '109',
+                'postalCode' => '179097',
+                'street' => 'North Bridge Road'
+            ),*/
+
+    'storePaymentMethod'=> true,
+    'shopperInteraction'=> 'Ecommerce',
+    'recurringProcessingModel'=> 'CardOnFile',
+    'shopperReference'=> 'Shopper03'
+
+    /*
+    'browserInfo' => [
+      'userAgent' => 'Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/70.0.3538.110 Safari\/537.36',
+      'acceptHeader' => "text\/html,application\/xhtml+xml,application\/xml;q=0.9,image\/webp,image\/apng,*\/*;q=0.8",
+      "language" => "en-US",
+      "colorDepth" => 24,
+        "screenHeight" => 723,
+        "screenWidth" => 1536,
+        "timeZoneOffset" => 0,
+      "javaEnabled" => true
+    ]*/
+];
+
+$final_payment_data = array_merge($payments_data, $additional_data);
+
+$curl_http_header = array(
+    "X-API-Key: AQEyhmfxLo/JYhZEw0m/n3Q5qf3VaY9UCJ1+XWZe9W27jmlZinFziP7eh81X6VPylhwbL0IQwV1bDb7kfNy1WIxIIkxgBw==-P0fsTdJtfItAU6POtaZObNDTJSVvm7749vgZ7dBXQ8M=-whdeW3vA2N6wvhtL",
+    "Content-Type: application/json"
+);
+
+$curl = curl_init();
+
+curl_setopt_array(
+    $curl,
+    [
+        CURLOPT_URL            => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST  => 'POST',
+        CURLOPT_POSTFIELDS     => json_encode($final_payment_data),
+        CURLOPT_HTTPHEADER     => $curl_http_header,
+        CURLOPT_VERBOSE        => true
+    ]
+);
+
+$payments_response = curl_exec($curl);
+$file = 'paymentsCallResponse.txt';
+$current = $payments_response;
+file_put_contents($file, $current);
+
+header('Content-Type: application/json');
+echo $payments_response;
+
+curl_close($curl);
+
+//header("location:index.php");
